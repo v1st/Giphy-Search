84eaecbf2ccd2a81e84d25d4aa89d5dc4ea0c806
Jaredm95 <jared97martinez@gmail.com>
2018-06-10 18:37:19 -0700

    Got Search working
    Got the Search working with the input box

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,18 @@ function getSearch(){
     let value;
 
     /* Collecting Value and calling Search Function */
-    value = document.getElementById("search");
+    value = $("#textbox").val();
 
     search(value, 0);
 }
 // Function for calling the API with the search
 function search(value, offset){
     /* Variables */
-    // API Key zfTPENJfHtscQrOBkg36COh4dQFUj9UG
     let newObject;
-
 
     /* XML Request */
     let xhr = new XMLHttpRequest(); 
-    xhr.open("GET", "https://api.giphy.com/v1/stickers/search?api_key=zfTPENJfHtscQrOBkg36COh4dQFUj9UG&q" + value 
-    + "=&limit=25&offset=" + offset + "&rating=G&lang=en", true); 
+    xhr.open("GET", "https://api.giphy.com/v1/stickers/search?api_key=zfTPENJfHtscQrOBkg36COh4dQFUj9UG&q=" + value + "&limit=25&offset=" + offset + "&rating=G&lang=en", true); 
     xhr.send();
     xhr.onreadystatechange = function () { 
         if (this.readyState === this.DONE) { 
